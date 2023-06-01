d0b48373c153a4c1c7835d7d8bd8f7cd6c504557
andrii <bondarenkoa1990@gmail.com>
2023-06-01 15:10:45 +0300

    FARM-2416: give qr code

--- src/screens/main/give-qr-code/GiveQrCode.tsx
+++ src/screens/main/give-qr-code/GiveQrCode.tsx
@@ -1,42 +1,69 @@
 import React, {useCallback, useState} from 'react';
 import {FlatList, TouchableOpacity, View} from 'react-native';
-import {Button, Searchbar, Text} from 'react-native-paper';
+import {Button, Divider, Searchbar, Text} from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import styles from 'src/screens/main/create-worker/styles';
+import styles from 'src/screens/main/give-qr-code/styles';
 import {Toast} from 'src/components/toast';
 import {strings} from 'src/locales/locales';
 import {useFarm} from 'src/stores/slices/auth.slice';
 import {getWorkersByName} from 'src/stores/services/firestore.service';
 import {FirestoreServiceError} from 'src/stores/errors';
-import {useNavigation} from '@react-navigation/native';
-import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/nativdee-stack';
 import {DrawerStackParamList} from 'src/navigation/drawer.stack';
 import {Worker} from 'src/stores/types/worker.type';
-const Item = ({worker}: {worker: Worker}) => (
-  <TouchableOpacity onPress={() => {}}>
-    <View>
-      <Text variant="headlineMedium">
-        {worker.firstName} {worker.middleName} {worker.lastName}
-      </Text>
-    </View>
-  </TouchableOpacity>
-);
+
+const Item = ({
+  handleSelectWorker,
+  worker,
+}: {
+  handleSelectWorker: (name: string) => void;
+  worker: Worker;
+}) => {
+  const name = `${worker.firstName}${
+    worker.middleName ? ` ${worker.middleName}` : ''
+  } ${worker.lastName}`;
+
+  return (
+    <TouchableOpacity
+      onPress={() => handleSelectWorker(name)}
+      style={styles.item}>
+      <View style={styles.titleItem}>
+        <Text variant="titleMedium">{name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const GiveQrCode = () => {
   const navigation =
     useNavigation<NativeStackNavigationProp<DrawerStackParamList>>();
-  const [searchQuery, setSearchQuery] = React.useState('');
-  const [workers, setWorkers] = React.useState<Array<Worker>>([]);
+  const [workers, setWorkers] = useState<Array<Worker>>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [canScanQrCode, setCanScanQrCode] = useState(false);
   const [errorMessage, setError] = useState('');
   const {firestorePrefix} = useFarm();
+  const handleSelectWorker = useCallback((name: string) => {
+    setSearchQuery(name);
+    setCanScanQrCode(true);
+    setWorkers([]);
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      setSearchQuery('');
+      setCanScanQrCode(false);
+      setWorkers([]);
+    }, []),
+  );
 
   const handleFindWorker = useCallback(
     async (name: string) => {
       setError('');
       setSearchQuery(name);
+      setCanScanQrCode(false);
       if (name === '') {
         setWorkers([]);
-        console.log(workers);
 
         return;
       }
@@ -48,7 +75,6 @@ const GiveQrCode = () => {
         );
 
         setWorkers(result);
-        console.log(workers);
       } catch (error: any) {
         console.log(error);
         if (error instanceof FirestoreServiceError) {
@@ -56,42 +82,56 @@ const GiveQrCode = () => {
         }
       }
     },
-    [firestorePrefix, workers, setWorkers],
+    [firestorePrefix, setWorkers],
   );
 
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={styles.container}>
         {errorMessage && <Toast error={errorMessage} />}
-        <View style={styles.wrapper}>
-          <View>
-            <Text variant="titleMedium">{strings.worker}</Text>
+        <View>
+          <Text variant="headlineSmall">{strings.worker}</Text>
+          <View style={styles.wrapper}>
             <Searchbar
               onChangeText={handleFindWorker}
               placeholder={strings.firstName}
-              style={{width: '100%'}}
+              style={styles.searchBar}
               testID="findName"
               value={searchQuery}
             />
-          </View>
-          <View>
+            <Divider />
             <FlatList
               data={workers}
               keyExtractor={item => `${item.uuid}`}
-              renderItem={({item}) => <Item worker={item} />}
+              renderItem={({item}) => (
+                <Item handleSelectWorker={handleSelectWorker} worker={item} />
+              )}
             />
-          </View>
-          <View style={{alignItems: 'center'}}>
-            <Button
-              disabled={!workers.length}
-              icon="qrcode"
-              mode="contained"
-              onPress={() => navigation.navigate('ScanQrCode')}
-              style={[styles.btn]}>
-              {strings.scanQrCode}
-            </Button>
+            {!workers.length && searchQuery && !canScanQrCode && (
+              <Text style={styles.workerNotFound} variant="titleMedium">
+                Работник не найден
+              </Text>
+            )}
+
+            {!workers.length && searchQuery && !canScanQrCode && (
+              <Text
+                onPress={() => navigation.navigate('CreateWorker')}
+                style={styles.linkCreateWorker}
+                variant="titleMedium">
+                + Зарегистрировать работника
+              </Text>
+            )}
           </View>
         </View>
+
+        <Button
+          disabled={!canScanQrCode}
+          icon="qrcode"
+          mode="contained"
+          onPress={() => navigation.navigate('ScanQrCode')}
+          style={[styles.btn]}>
+          {strings.scanQrCode}
+        </Button>
       </View>
     </SafeAreaView>
   );
