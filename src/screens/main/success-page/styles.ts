bce8640f48b965c04fd9e47e3cf2d3362ff6b5f7
andrii <bondarenkoa1990@gmail.com>
2023-06-06 19:43:55 +0300

    small fixes

--- src/screens/main/success-page/styles.ts
+++ src/screens/main/success-page/styles.ts
@@ -5,8 +5,8 @@ export const width = Dimensions.get('window').width / 3 - 20;
 
 export default StyleSheet.create({
   container: {
+    backgroundColor: colors.background,
     flex: 1,
-    margin: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -15,11 +15,19 @@ export default StyleSheet.create({
     borderColor: colors.black,
     borderRadius: 10,
     borderWidth: 1,
-    width: '50%',
+    width: '70%',
     margin: 10,
     height: '40%',
     alignItems: 'center',
     justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+    elevation: 12,
   },
   titleText: {
     color: colors.black,
