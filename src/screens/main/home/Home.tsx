23a632ca33b503dcd54a4e53dadce7801d26c492
andrii <bondarenkoa1990@gmail.com>
2023-07-11 19:54:26 +0300

    hotfix

--- src/screens/main/home/Home.tsx
+++ src/screens/main/home/Home.tsx
@@ -43,7 +43,7 @@ const Home = () => {
           );
         })}
       </View>
-      <Snackbar onDismiss={() => {}} visible={!netState.isConnected}>
+      <Snackbar onDismiss={() => {}} visible={!netState.isInternetReachable}>
         <Text style={styles.snackbar}>{strings.appWorksOffline}</Text>
       </Snackbar>
     </SafeAreaView>
