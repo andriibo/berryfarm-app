b568305d58b401225487b22aee68d1f51e27817f
andrii <bondarenkoa1990@gmail.com>
2023-07-14 10:39:23 +0300

    FARM-2448: small fixes

--- src/stores/hooks/use-check-internet-connection.ts
+++ src/stores/hooks/use-check-internet-connection.ts
@@ -1,20 +1,16 @@
 import NetInfo from '@react-native-community/netinfo';
 import {useEffect} from 'react';
 import {setIsInternetConnection} from 'src/stores/slices/connect-device.slice';
-
 import {useAppDispatch} from './hooks';
+import {NetInfoStateType} from '@react-native-community/netinfo/src/internal/types';
 
 export const useCheckInternetConnection = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener(state => {
-      if (state.isConnected && state.type === 'wifi') {
-        if (state.isInternetReachable === false) {
-          dispatch(setIsInternetConnection(state.isInternetReachable));
-        } else {
-          dispatch(setIsInternetConnection(state.isConnected));
-        }
+      if (state.isConnected && state.type === NetInfoStateType.wifi) {
+        dispatch(setIsInternetConnection(!!state.isInternetReachable));
       } else {
         dispatch(setIsInternetConnection(!!state.isConnected));
       }
